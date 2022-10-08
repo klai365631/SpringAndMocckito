1c4d80e2f7da1700e7725c01f3b672fe1bbf0c18
ANDRE <klai1@mail.ru>
2022-10-08 23:55:31 +0400

    DZ

--- src/test/java/com/SkyPro/SpringAndMocckito/DepartmentServiceTest.java
+++ src/test/java/com/SkyPro/SpringAndMocckito/DepartmentServiceTest.java
@@ -14,7 +14,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
